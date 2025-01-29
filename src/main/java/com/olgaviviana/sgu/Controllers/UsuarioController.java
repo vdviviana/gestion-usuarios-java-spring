2194030135b511ec05b17665016f61f4758b977e
vdviviana <vdviviana@hotmail.com>
2025-01-29 18:50:31 -0300

    Actualizando proyecto sgu v1

---
+++ src/main/java/com/olgaviviana/sgu/Controllers/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.olgaviviana.sgu.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value = "prueba")
+    public List<String> prueba() {
+        return List.of("manzana","kiwi","banana");
+    }
+}
---
+++ src/test/java/com/olgaviviana/sgu/SguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olgaviviana.sgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
